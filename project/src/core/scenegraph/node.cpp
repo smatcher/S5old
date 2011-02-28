9c9e4b39510c868c8a645e8631bafe52b2dbfe91
Hautbois <bhautboi@gmail.com>
2011-02-28 10:41:35 +0100

    Changé GL_LINE en GL_LINES, ca devrait corriger le bug avec l'affichage des repères

--- project/src/core/scenegraph/node.cpp
+++ project/src/core/scenegraph/node.cpp
@@ -19,17 +19,17 @@ void Node::drawTransform(const GLWidget* widget, bool recursive) const
 		this->glMultd();
 
 		widget->qglColor(Qt::red);
-		glBegin(GL_LINE);
+		glBegin(GL_LINES);
 			glVertex3d(0,0,0);
 			glVertex3d(1,0,0);
 		glEnd();
 		widget->qglColor(Qt::green);
-		glBegin(GL_LINE);
+		glBegin(GL_LINES);
 			glVertex3d(0,0,0);
 			glVertex3d(0,1,0);
 		glEnd();
 		widget->qglColor(Qt::blue);
-		glBegin(GL_LINE);
+		glBegin(GL_LINES);
 			glVertex3d(0,0,0);
 			glVertex3d(0,0,1);
 		glEnd();
