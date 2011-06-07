20b86147266c024714e3f693c0ba27c810a36d55
Hautbois <bhautboi@gmail.com>
2011-06-07 10:21:32 +0200

    Timing du temps de chargement

--- S5engine/include/core/resources/managers.h
+++ S5engine/include/core/resources/managers.h
@@ -19,6 +19,10 @@
 
 #include "core/utils/primitives.h"
 
+#include "core/log/log.h"
+
+#include <QDateTime>
+
 #define MEDIA_DIR "media/"
 
 typedef Singleton< ResourceManager< MeshData,           Mesh> >           SingletonMeshManager;
@@ -46,33 +50,65 @@ inline void initResourceManagers(QString spec_dir = "")
 
 	QString base_dir = MEDIA_DIR;
 
+	QDateTime firstTime = QDateTime::currentDateTime();
+	QDateTime lastTime = QDateTime::currentDateTime();
+	QDateTime time;
+
 	MESH_MANAGER.parseDir(base_dir + "models",true);
 	if(spec_dir != "") MESH_MANAGER.parseDir(spec_dir + "models",true);
 	MESH_MANAGER.loadAll();
+
+	time = QDateTime::currentDateTime();
+	debug("LOADING TIMING","Loading meshes" << double(lastTime.msecsTo(time))/1000.0 << "seconds")
+	lastTime = time;
 
 	TEXTURE_MANAGER.parseDir(base_dir + "textures",true);
 	if(spec_dir != "") TEXTURE_MANAGER.parseDir(spec_dir + "textures",true);
 	TEXTURE_MANAGER.loadAll();
 
+	time = QDateTime::currentDateTime();
+	debug("LOADING TIMING","Loading textures" << double(lastTime.msecsTo(time))/1000.0 << "seconds")
+	lastTime = time;
+
 	SAMPLE_MANAGER.parseDir(base_dir + "sounds",true);
 	if(spec_dir != "") SAMPLE_MANAGER.parseDir(spec_dir + "sounds",true);
 	SAMPLE_MANAGER.loadAll();
+
+	time = QDateTime::currentDateTime();
+	debug("LOADING TIMING","Loading samples" << double(lastTime.msecsTo(time))/1000.0 << "seconds")
+	lastTime = time;
 
 	SHADER_MANAGER.parseDir(base_dir + "shaders",true);
 	if(spec_dir != "") SHADER_MANAGER.parseDir(spec_dir + "shaders",true);
 	SHADER_MANAGER.loadAll();
 
+	time = QDateTime::currentDateTime();
+	debug("LOADING TIMING","Loading shaders" << double(lastTime.msecsTo(time))/1000.0 << "seconds")
+	lastTime = time;
+
 	SHADER_PROGRAM_MANAGER.parseDir(base_dir + "shaders",true);
 	if(spec_dir != "") SHADER_PROGRAM_MANAGER.parseDir(spec_dir + "shaders",true);
 	SHADER_PROGRAM_MANAGER.loadAll();
+
+	time = QDateTime::currentDateTime();
+	debug("LOADING TIMING","Loading shader programs" << double(lastTime.msecsTo(time))/1000.0 << "seconds")
+	lastTime = time;
 
 	MATERIAL_MANAGER.parseDir(base_dir + "materials",true);
 	if(spec_dir != "") MATERIAL_MANAGER.parseDir(spec_dir + "materials",true);
 	MATERIAL_MANAGER.loadAll();
 
+	time = QDateTime::currentDateTime();
+	debug("LOADING TIMING","Loading materials" << double(lastTime.msecsTo(time))/1000.0 << "seconds")
+	lastTime = time;
+
 	MESH_MANAGER.add(PrimitiveMesh::buildCube());
 	MESH_MANAGER.add(PrimitiveMesh::buildPlane());
 	MESH_MANAGER.add(PrimitiveMesh::buildSphere(16,32));
+
+	time = QDateTime::currentDateTime();
+	debug("LOADING TIMING","Total time" << double(firstTime.msecsTo(time))/1000.0 << "seconds")
+	lastTime = time;
 }
 
 #endif // MANAGERS_H
