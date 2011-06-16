1a47293101c25cf4790c8ddd8a536f220e7d1cd8
Addict7 <nicolasfloquet@gmail.com>
2011-06-16 21:14:09 +0200

    jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/jour/nuit/

--- S5engine/src/core/properties/dummyupdatable.cpp
+++ S5engine/src/core/properties/dummyupdatable.cpp
@@ -10,5 +10,5 @@ void DummyUpdatable::update(double elapsed_time)
 {
 	//std::cout << "Updating DummyUpdatable" << std::endl;
 	Node* parentNode = this->node();
-	parentNode->rotate(Vector3f(0.0, 1.0, 0.0), angle*elapsed_time);
+	parentNode->rotate(Vector3f(0.0, 1.0, 0.0), angle*elapsed_time*0.1);
 }
--- demo_game/test_terrain.cpp
+++ demo_game/test_terrain.cpp
@@ -41,6 +41,7 @@ int main(int argc, char *argv[])
 	SceneGraph* sg = engine.getScenegraph_TEMPORARY();
 
 	Node* nTerrain = new Node("Terrain");
+	Node* nBase = new Node("Base");
 	Node* nRot = new Node("Rotating node");
 	Node* nLight = new Node("Light");
 
@@ -52,30 +53,38 @@ int main(int argc, char *argv[])
 		heightmap = TEXTURE_MANAGER.get("heightmap.tga");
 	}
 
+
+	Mesh sphere = MESH_MANAGER.get("Sphere_16_32");
+	Material mat = MATERIAL_MANAGER.get("unicorn");
 	Material terrain = MATERIAL_MANAGER.get("terrain_test");
 
-	nTerrain->addProperty(new TerrainRenderer(heightmap, terrain, 100.0f, 1.f,20.f));
+	nTerrain->addProperty(new TerrainRenderer(heightmap, terrain, 70.0f, 1.f,20.f));
 	nTerrain->move(Vector3f(-256.f, -100.f, -256.f));
 
 	nLight->addProperty(new Light());
-	nLight->moveTo(Vector3f(200.0f, 50.0f, 200.0f));
+	nLight->addProperty(new MeshRenderer(sphere,mat));
+	nLight->moveTo(Vector3f(512.0f, 0.0f, 0.0f));
 
-	////nRot->addProperty(new DummyUpdatable());
-	nRot->setScale(Vector3f(20.0f, 20.0f, 20.0f));
+	nRot->addProperty(new DummyUpdatable());
+
+	nBase->rotate(Vector3f(1,0,0),90);
+	nBase->rotate(Vector3f(0,1,0),-45);
+
 	sg->link(nTerrain);
-	//nRot->link(nLight);
-	sg->link(nLight);
-	sg->link(nRot);
+	nRot->link(nLight);
+	nBase->link(nRot);
+	sg->link(nBase);
 
 	RenderManager::Background background;
-	background.type = RenderManager::SKYBOX;
-	background.color = Vector3f(1,1,0);
-	background.textures[0] = TEXTURE_MANAGER.get("stormy_front.tga");
+	background.type = RenderManager::COLOR;
+	background.color = Vector3f(0,0,0);
+/*	background.textures[0] = TEXTURE_MANAGER.get("stormy_front.tga");
 	background.textures[1] = TEXTURE_MANAGER.get("stormy_left.tga");
 	background.textures[2] = TEXTURE_MANAGER.get("stormy_back.tga");
 	background.textures[3] = TEXTURE_MANAGER.get("stormy_right.tga");
 	background.textures[4] = TEXTURE_MANAGER.get("stormy_top.tga");
 	background.textures[5] = TEXTURE_MANAGER.get("stormy_bottom.tga");
+	*/
 	/*
 	background.textures[0] = TEXTURE_MANAGER.get("interstellar_lf.tga");
 	background.textures[1] = TEXTURE_MANAGER.get("interstellar_ft.tga");
